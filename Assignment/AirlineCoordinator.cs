0b61a7cb06e482faa7d7b9d620592d2362c5b791
Felix Varming Sandgren <feli0100@stud.kea.dk>
2019-11-07 22:14:18 +0100

    Add project files.

---
+++ Assignment/AirlineCoordinator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment
+{
+    class AirlineCoordinator
+    {
+        FlightManager flManager;
+
+        CustomerManager custManager;
+
+
+
+
+
+        public AirlineCoordinator(int custIdSeed, int maxCust, int maxFlights)
+
+        {
+
+            flManager = new FlightManager(maxFlights);
+
+            custManager = new CustomerManager(custIdSeed, maxCust);
+
+        }
+
+
+
+        public bool addFlight(int flightNo, string origin, string destination, int maxSeats)
+
+        {
+
+            return flManager.addFlight(flightNo, origin, destination, maxSeats);
+
+        }
+
+
+
+        public bool addCustomer(string fname, string lname, string phone)
+
+        {
+
+            return custManager.addCustomer(fname, lname, phone);
+
+        }
+
+
+
+        public string flightList()
+
+        {
+
+            return flManager.getFlightList();
+
+        }
+
+
+
+        public string customerList()
+
+        {
+
+            return custManager.getCustomerList();
+
+        }
+
+
+
+        public bool deleteCustomer(int id)
+
+        {
+
+            return custManager.deleteCustomer(id);
+
+        }
+
+
+
+        public bool deleteFlight(int fid)
+
+        {
+
+            return flManager.deleteFlight(fid);
+
+        }
+    }
+}
